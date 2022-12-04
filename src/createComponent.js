b31914785e0312a10a2fff90b0def53918c39549
anwar254 <sadatt254@gmail.com>
2022-12-04 16:20:13 +0300

    crated the copy functionality

--- src/createComponent.js
+++ src/createComponent.js
@@ -6,18 +6,49 @@ import Listr from 'listr';
 import { toCamelCase } from './file-formatting';
 import { isDirSync } from './syncdir';
 import { baseFile } from '../templates/base';
+import component from '../templates/component';
 
-async function createThemeFiles(options){
+async function createThemeFiles(options, copy){
     const targetWorkingDirectory = options.targetDirectory;
     const fileFormated = toCamelCase(options.name);
     const baseName = `${fileFormated}.js`;
+    const fileName = `${fileFormated}.js`;
     const baseDir = path.resolve(path.join(targetWorkingDirectory, `/src/elements/base/${baseName}`));
+    const componentsPath = path.resolve(path.join(targetWorkingDirectory, `/src/theme/${options.theme}/components/elements/`))
+    const fileDir = path.resolve(path.join(targetWorkingDirectory, `/src/themes/${options.theme}/components/elements/${fileName}`));
     
     // Creating basefile
     if(isDirSync(path.resolve(path.join(targetWorkingDirectory, '/src/elements/base/')))){
         fs.createWriteStream(baseDir);
     }else{
         console.log(false)
+    }
+
+    // Creating component files
+    try {
+        if (isDirSync(path.resolve(path.join(targetWorkingDirectory, `/src/themes/${options.theme}/components/elements/`)))){
+            if (options.name.split('-').length > 4){
+                console.log('Naming error');
+                process.error(1)
+            }else{
+                if(copy){
+                    fs.createWriteStream(fileDir);
+
+                    fs.writeFile(fileDir, component(options.name), function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
+                        console.log("The file was saved!");
+                    }); 
+
+                }
+                fs.createWriteStream(fileDir);
+            }
+        }else {
+            console.log('the directory does not exist')
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -31,6 +62,11 @@ export async function createComponent(options) {
         {
             title: 'Creating files',
             task: () => createThemeFiles(options)
+        },
+        {
+            title: 'Copying file\s content',
+            task: () => createThemeFiles(options, options.copy),
+            enabled: () => options.copy,
         }
     ])
 
